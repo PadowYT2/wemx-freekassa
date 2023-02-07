1d87bfc0567f78176de492ada32fa93e0248a4ad
PadowYT2 <me@padow.ru>
2023-02-07 18:58:30 +0300

    any -> post

--- routes/custom/freekassa.php
+++ routes/custom/freekassa.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\Billing\Gateways\FreekassaGateway;
 
-Route::any('/gateway/freekassa/checkout', [FreekassaGateway::class, 'checkout'])->name('freekassa.checkout');
+Route::post('/gateway/freekassa/checkout', [FreekassaGateway::class, 'checkout'])->name('freekassa.checkout');
 
 Route::group(['middleware' => 'guest'], function () {
-    Route::any('/remote/freekassa/webhook', [FreekassaGateway::class, 'callback'])->name('freekassa.callback')->withoutMiddleware(['web', 'auth', 'csrf']);
+    Route::post('/remote/freekassa/webhook', [FreekassaGateway::class, 'callback'])->name('freekassa.callback')->withoutMiddleware(['web', 'auth', 'csrf']);
 });
